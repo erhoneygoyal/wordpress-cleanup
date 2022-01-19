8f79e8a9a460be0ce1aaedfbf5dc1311a954e810
Er Honey Goyal <honeygyls@gmail.com>
2022-01-19 15:51:39 -0400

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+// //remove emoji support
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+// // Remove rss feed links
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+
+// // remove wp-embed
+add_action( 'wp_footer', function(){
+    wp_dequeue_script( 'wp-embed' );
+});
+
+
+add_action( 'wp_enqueue_scripts', function(){
+    // // remove block library css
+    wp_dequeue_style( 'wp-block-library' );
+    // // remove comment reply JS
+    wp_dequeue_script( 'comment-reply' );
+} );
+
+
+// for wp-config.php
+// define( 'CORE_UPGRADE_SKIP_NEW_BUNDLED', true );
+// define('WP_POST_REVISIONS', false);
+// define('WP_POST_REVISIONS', 5);
